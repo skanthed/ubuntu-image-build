8f4a1e9a7c9827b98ed9d6acf894fbedee14d81a
Lars Kellogg-Stedman <lars@oddbit.com>
2024-10-31 15:59:17 -0400

    Fix UEFI boot issues
    This adds a custom element to re-run grub-install without the
    `--removeable` option, which causes it to correctly populate the
    `/boot/efi/EFI/ubuntu` directory. This results in an image that will boot
    successfully on UEFI systems.
    
    You will run into an issue testing this image in a virtual machine: when
    the image first boots, it immediately resets during the early UEFI boot
    process. This will require you to `virsh start` the image, which will work
    fine but will not attach cloud-init metadata you defined with the
    `--cloud-init` option so you won't be able to log in.
    
    Consider using `virt-customize` to set a root password before testing the
    image.

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -8,5 +8,17 @@ export DIB_GRUB_INSTALLTYPE=both
 export DIB_DISTRIBUTION_MIRROR=http://archive.ubuntu.com/ubuntu/
 export IMG=ubuntu24.04-uefi-legacy-new.qcow2
 export DIB_BASE_IMAGE_URL=https://cloud-images.ubuntu.com/noble/current/ubuntu-noble-server-cloudimg-amd64-disk-kernel.img
+export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive,NoCloud,None"
+export DIB_BLOCK_DEVICE=efi
+export ELEMENTS_PATH=./elements
 
-disk-image-create ubuntu vm block-device-efi dhcp-all-interfaces bootloader grub2 -o $IMG -p linux-firmware,cloud-init
+disk-image-create \
+  ubuntu \
+  vm \
+  block-device-efi \
+  dhcp-all-interfaces \
+  bootloader \
+  grub2 \
+  cloud-init-datasources \
+  reinstall-grub-efi \
+  -o $IMG -p linux-firmware,cloud-init
